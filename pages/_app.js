4940c73800ff2ad9fc2119744edcc9db13e893ed
AmauriMarcos <amaurisantos.m@gmail.com>
2021-09-07 21:05:55 +0200

    add header to app component

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,17 @@
 import "../styles/global.scss";
 import Layout from "../component/Layout";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
+      <Head>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0, maximum-scale=1.0,user-scalable=0"
+        />
+        <title>Amauri Santos</title>
+      </Head>
       <Layout>
         <Component {...pageProps} />
       </Layout>
