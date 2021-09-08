a87186287313b3144948965dbd6466a60dc86114
AmauriMarcos <amaurisantos.m@gmail.com>
2021-09-08 13:14:37 +0200

    add a wrapper to the navbar

--- component/Layout.js
+++ component/Layout.js
@@ -16,17 +16,16 @@ export default function Layout({ children }) {
   return (
     <React.Fragment>
 
-      <Head>
-      <link href="https://fonts.googleapis.com/css2?family=Roboto+Condensed:wght@400;700&display=swap" rel="stylesheet"></link>
-      </Head>
-
-      {isDesktopOrLaptop && <Navbar/>}
-      {isTabletOrMobile && <MobileNavbar/>}
+      <div>
+        {isDesktopOrLaptop && <Navbar/>}
+        {isTabletOrMobile && <MobileNavbar/>}
+      </div>
       
       <div className={styles.container}>
-        <main>{children}</main>
-        <Footer/>
+        <main>{children}</main>  
       </div>
+
+      <Footer/>
       
     </React.Fragment>
   );
--- pages/_app.js
+++ pages/_app.js
@@ -6,6 +6,7 @@ function MyApp({ Component, pageProps }) {
   return (
     <>
       <Head>
+        <link href="https://fonts.googleapis.com/css2?family=Roboto+Condensed:wght@400;700&display=swap" rel="stylesheet"></link>
         <meta
           name="viewport"
           content="width=device-width, initial-scale=1.0, maximum-scale=1.0,user-scalable=0"
